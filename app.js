4458c5da5c33bb137482511db7b0fcb475c34497
jamilecadias <jamilecadias@gmail.com>
2022-06-10 23:56:32 -0300

    Agregando Procfile

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ app.use(express.static('public'));
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Servidor corriendo en el puerto 3000');
-})
+});
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
