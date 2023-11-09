be705a0108a75f1daf41fbfb3310c2715ad99504
jamilecadias <jamilecadias@gmail.com>
2023-11-09 17:04:15 -0300

    configurando

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || 3000, function() {
+/* app.listen(process.env.PORT || 3000, function() {
     console.log('Servidor corriendo en el puerto 3000');
-});
+}); */
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
+
+
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/index.html');
